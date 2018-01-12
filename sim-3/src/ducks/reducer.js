98b5cba3720863c38da28a653a754371b24c8239
Austin Randall <randall@Randall.local>
2018-01-12 16:50:40 -0700

    redux is working and updating

--- sim-3/src/ducks/reducer.js
+++ sim-3/src/ducks/reducer.js
@@ -1,15 +1,27 @@
 const initialState = {
-    user: null
+    user: 'Austin',
+    firstName: 'Austin',
+    lastName: 'Randall',
+    picture: 'https://robohash.org/me',
 }
 
 //action type
 const LOGIN = "LOGIN";
+const FIRSTNAME = "FIRSTNAME";
+const LASTNAME = "LASTNAME";
 
 //reducer
 export default function reducer(state = initialState, action) {
     switch (action.type) {
         case LOGIN:
             return { ...state, user: action.payload }
+
+        case FIRSTNAME:
+            return { ...state, firstName: action.payload }
+
+        case LASTNAME:
+            return { ...state, lastName: action.payload }
+
         default: 
             return state;
     }
@@ -20,5 +32,19 @@ export function login(user) {
     return {
         type: LOGIN,
         payload: user
+    }
+}
+
+export function updateName(firstName) {
+    return {
+        type: FIRSTNAME,
+        payload: firstName
+    }
+}
+
+export function updateLastName(lastName) {
+    return {
+        type: LASTNAME,
+        payload: lastName
     }
 }
